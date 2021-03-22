d6af91daebaaca62717a69c5acb180dabb58b706
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-03-23 07:07:00 +0900

    不必要なインデントの削除

--- shell/echo.sh
+++ shell/echo.sh
@@ -2,22 +2,22 @@
 
 #シェルスクリプト作成（echoコマンドを利用する）
 
-	echo 'hello-world'
-	echo '出力成功'
+echo 'hello-world'
+echo '出力成功'
 
 #変数に値を代入して出力
 
-	name="yamada"
-	echo $name
-	echo "${name}taro"
+name="yamada"
+echo $name
+echo "${name}taro"
 
 #変数の値の上書き
 
-	name="tanaka"
-	name="sato"
-	echo $name
+name="tanaka"
+name="sato"
+echo $name
 
 #エラーを出す(=の間に半角スペースを入れる)
 
-	error = 'hoge'
-	echo $error
+error = 'hoge'
+echo $error

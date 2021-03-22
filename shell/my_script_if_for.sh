979d75607bb4f5674697be9f603d4ee95ed9208e
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-03-22 12:22:53 +0900

    シェルスクリプトの作成（if文とfor文を利用した処理をする)

---
+++ shell/my_script_if_for.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#シェルスクリプト作成（if文とfor文を利用した処理をする)
+
+read -p '名前を入力してください:' name
+
+if [[ -z $name ]]; then
+	echo '空です'
+fi
+
+if [[ $name == 'tanaka' ]]; then
+	for ((i=1; i<=10; i++))
+	do
+		echo $name
+	done
+else
+	echo '名前が一致しませんでした'
+fi

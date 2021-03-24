c623de5b6fc3700511b5ed912222151f34498ea6
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-03-25 01:00:54 +0900

    スペース＆改行の修正

--- shell/number.sh
+++ shell/number.sh
@@ -5,36 +5,43 @@
 read -p '数値を入力してください：' number
 
 #入力した数値aが数値bと等しい場合(==)
+
 if ((number == 1)); then
   echo "${number}は1と等しいです"
 fi
 
 #入力した数値aが数値bと等しくない場合(!=)
-if ((number != 2));  then
+
+if ((number != 2)); then
   echo "${number}は2と等しくありません"
 fi
 
 #入力した数値aが数値b以上の場合(>=)
-if ((number >= 3));  then
+
+if ((number >= 3)); then
   echo "${number}は3以上です"
 fi
 
 #入力した数値aが数値b以下の場合(<=)
-if ((number <= 4));  then
+
+if ((number <= 4)); then
   echo "${number}は4以下です"
 fi
 
 ##入力した数値aが数値bより大きい場合(>)
-if ((number > 5));  then
+
+if ((number > 5)); then
   echo "${number}は5より大きいです"
 fi
 
 ##入力した数値aが数値bより小さい場合(<)
-if ((number < 6));  then
+
+if ((number < 6)); then
   echo "${number}は6より小さいです"
 fi
 
 #else文、elseif文
+
 if ((number < 8)); then
   echo "${number}は8より小さい"
 elif ((number == 8)); then

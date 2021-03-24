4816f0ef0cb12459ed8352f7f80eb4412bad3629
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-03-25 00:56:04 +0900

    改行の修正

--- shell/echo.sh
+++ shell/echo.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 #シェルスクリプト作成（echoコマンドを利用する）
+
 echo 'hello-world'
 echo '出力成功'
 
 #変数に値を代入して出力
+
 name="yamada"
 echo $name
 echo "${name}taro"
 
 #変数の値の上書き
+
 name="tanaka"
 name="sato"
 echo $name
 
 #エラーを出す(=の間に半角スペースを入れる)
+
 error = 'hoge'
 echo $error

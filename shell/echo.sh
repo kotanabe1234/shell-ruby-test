15610e1ba9122f4602f448f4d1205c437965f1cc
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-03-25 00:48:57 +0900

    インデントの修正

--- shell/echo.sh
+++ shell/echo.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 #シェルスクリプト作成（echoコマンドを利用する）
-
 echo 'hello-world'
 echo '出力成功'
 
 #変数に値を代入して出力
-
 name="yamada"
 echo $name
 echo "${name}taro"
 
 #変数の値の上書き
-
 name="tanaka"
 name="sato"
 echo $name
 
 #エラーを出す(=の間に半角スペースを入れる)
-
 error = 'hoge'
 echo $error

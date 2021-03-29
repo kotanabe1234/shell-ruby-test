8b8f35dd80c8e8265a5898b0484d72adaac4090f
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-03-29 22:20:05 +0900

    if文は[[]]or[]を使うように修正

--- shell/number.sh
+++ shell/number.sh
@@ -2,50 +2,48 @@
 
 #シェルスクリプト作成（if文で数値の比較をする）
 
-read -p '数値を入力してください：' number
-
 #入力した数値aが数値bと等しい場合(==)
 
-if ((number == 1)); then
-  echo "${number}は1と等しいです"
+if [[ $1 == 1 ]]; then
+  echo "${1}は1と等しいです"
 fi
 
 #入力した数値aが数値bと等しくない場合(!=)
 
-if ((number != 2)); then
-  echo "${number}は2と等しくありません"
+if [[ $1 != 2 ]]; then
+  echo "${1}は2と等しくありません"
 fi
 
-#入力した数値aが数値b以上の場合(>=)
+#入力した数値aが数値b以上の場合(-ge)
 
-if ((number >= 3)); then
-  echo "${number}は3以上です"
+if [[ $1 -ge 3 ]]; then
+  echo "${1}は3以上です"
 fi
 
-#入力した数値aが数値b以下の場合(<=)
+#入力した数値aが数値b以下の場合(-le)
 
-if ((number <= 4)); then
-  echo "${number}は4以下です"
+if [[ $1 -le 4 ]]; then
+  echo "${1}は4以下です"
 fi
 
-##入力した数値aが数値bより大きい場合(>)
+#入力した数値aが数値bより大きい場合(>)
 
-if ((number > 5)); then
-  echo "${number}は5より大きいです"
+if [[ $1 > 5 ]]; then
+  echo "${1}は5より大きいです"
 fi
 
-##入力した数値aが数値bより小さい場合(<)
+#入力した数値aが数値bより小さい場合(<)
 
-if ((number < 6)); then
-  echo "${number}は6より小さいです"
+if [[ $1 < 6 ]]; then
+  echo "${1}は6より小さいです"
 fi
 
 #else文、elseif文
 
-if ((number < 8)); then
-  echo "${number}は8より小さい"
-elif ((number == 8)); then
-  echo "${number}は8と等しい"
+if [[ $1 < 8 ]]; then
+  echo "${1}は8より小さい"
+elif [[ $1 == 8 ]]; then
+  echo "${1}は8と等しい"
 else
-  echo "${number}は8より大きいです"
+  echo "${1}は8より大きいです"
 fi

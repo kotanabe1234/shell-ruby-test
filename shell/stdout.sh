6eff6e94cf3819e9086cb49e5bc4f593c0e64b2f
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-04-03 03:46:40 +0900

    CheckPoint部分は削除し、標準出力をファイルにリダイレクトするコードに修正

--- shell/stdout.sh
+++ shell/stdout.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 #シェルスクリプト作成(標準出力(STDOUT)をファイルにリダイレクトする)
-#CheckPoint
-#コマンドライン引数に"ok"を渡すと標準出力(文字列は任意)をok.txtへリダイレクト
 
-#シェルスクリプト作成（okへリダイレクト)
-if [[ -z $1 ]]; then
-  echo '引数が入力されていません'
-  exit;
-fi
-
-if [[ $1 != 'ok' ]]; then
-  echo 'コマンドライン引数にはokと入力してください'
-  exit;
-fi
-
-echo 'リダイレクトに成功しました' > ok.txt
+echo '標準出力がリダイレクトされました' > redirect.txt
+echo 'リダイレクトに成功しました' > test.txt
+echo '成功しました' 1> practice.txt

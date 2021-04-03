13e1117840c95016c9b2cde51adcff97a8dc4bce
kotanabe1234 <kotanabetaro1234@gmail.com>
2021-04-03 23:37:38 +0900

    シェルスクリプトを呼び出す実行方法の追記

--- shell/call.sh
+++ shell/call.sh
@@ -4,3 +4,27 @@
 
 test=`./test.sh`
 echo "$test"
+
+#↑sourceを使用して呼び出していない場合、
+#test.shが変数を定義するスクリプトの場合変数が使用できない
+echo "$hoge"
+echo "$foo"
+
+#sourceを使用すると定義される(. ./test or source ./test.sh)
+
+. ./test.sh
+echo "$hoge"
+echo "$foo"
+
+#関数を使用してみる
+
+source test.sh
+
+name
+
+val=$(calculation $1)
+echo $?
+
+
+
+
